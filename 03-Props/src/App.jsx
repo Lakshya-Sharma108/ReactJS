c7e6b888510babd0831be4370ff336a87e05f00c
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-24 10:21:44 +0530

    feat: add image prop to Card component and update layout in App component

--- 03-Props/src/App.jsx
+++ 03-Props/src/App.jsx
@@ -6,9 +6,9 @@ const App = () => {
     <div className='parent'>
       
       {/* Here we are passing props to the Card component -- name, age, description are the props */}
-      <Card name='Lakshaya Awasthi' age={21} description='I am a software engineer.' />
-      <Card name='Yogesh' age={28} description='I am a AI engineer.' />
-      <Card name='Deependra' age={32} description='I am a fucking asshole.' />
+      <Card img="https://images.unsplash.com/photo-1617972882867-3707f274c115?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" name='Lakshaya Awasthi' age={21} description='I am a software engineer.' />
+      <Card img="https://images.unsplash.com/photo-1773633071680-a1b53fcb4e90?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" name='Yogesh' age={28} description='I am a AI engineer.' />
+      <Card img="https://images.unsplash.com/photo-1773408151429-4add3b61d7bb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" name='Deependra' age={32} description='I am a fucking asshole.' />
     </div>
   )
 }
--- 03-Props/src/components/Card.jsx
+++ 03-Props/src/components/Card.jsx
@@ -7,9 +7,8 @@ const Card = (props) => {
 
   return (
     <div className="card">
-        <img src="https://images.unsplash.com/photo-1743105351262-3f9e6944920a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>{props.name}</h1>
-        <h4>Age: {props.age}</h4>
+        <img src={props.img} alt="" />
+        <h1>{props.name}, {props.age}</h1>
         <p>{props.description}</p>
         <button>View Profile</button>
       </div>  
