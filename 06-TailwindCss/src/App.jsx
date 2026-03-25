e7e83ebd7a78b3054ee468c71b823456dbde934a
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-25 10:45:55 +0530

    feat: update App component to include Tailwind CSS styling and animated greeting

--- 06-TailwindCss/src/App.jsx
+++ 06-TailwindCss/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-900 text-white'>
-      <h1>Hello ji</h1>
+    // Tailwind CSS is a utility-first CSS framework that provides a set of pre-defined classes to help you quickly build custom designs without writing custom CSS. It allows you to style your components directly in your HTML or JSX using class names that correspond to specific styles.
+    <div className='h-screen w-full bg-gray-900 text-white  flex items-center justify-center font-bold text-4xl'>
+      <h1 className='animate-bounce'>Hello, Lakshaya Awasthi</h1>
     </div>
   )
 }
