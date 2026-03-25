4d1efe68114f78576b28e2a05a702fa2a5de84e1
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-25 10:38:17 +0530

    feat: add Tailwind CSS setup with initial components and assets
    - Created SVG icons for Bluesky, Discord, Documentation, GitHub, Social, and X.
    - Added App.css for styling components with Tailwind CSS.
    - Implemented App.jsx as the main application component.
    - Included hero image and React logo SVG assets.
    - Set up index.css to import Tailwind CSS.
    - Developed main.jsx to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ 06-TailwindCss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 text-white'>
+      <h1>Hello ji</h1>
+    </div>
+  )
+}
+
+export default App
