b11f70f693c811d6be1c174660d3ab2039741f6c
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-26 17:16:26 +0530

    feat: update Section2 component layout and styling; change background color and add title and subtitle

--- 07-UI-Project/src/components/Section2/Section2.jsx
+++ 07-UI-Project/src/components/Section2/Section2.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Section2 = () => {
   return (  
-    <div className='h-screen w-full bg-gray-950'>
-      
+    <div className='h-screen w-full bg-blue-950 flex flex-col justify-center items-center gap-3'>
+      <h1 className='text-white font-bold text-6xl'>Lakshaya Awasthi</h1>
+      <p className='text-gray-400 text-lg mt-4 '>Software Engineer</p>
     </div>
   )
 }
