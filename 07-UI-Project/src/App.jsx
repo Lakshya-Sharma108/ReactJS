32e42c6232a3b0d0c5d1e1f1594ee631b9e95fc3
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-26 16:15:05 +0530

    feat: refactor RightCard and RightCardContent components; add Users array to App component

--- 07-UI-Project/src/App.jsx
+++ 07-UI-Project/src/App.jsx
@@ -3,6 +3,24 @@ import Section1 from './components/Section1/Section1'
 import Section2 from './components/Section2/Section2'
 
 const App = () => {
+  const Users = [
+    {
+      img: 'https://i.pinimg.com/736x/af/5c/72/af5c72b04d1331cc3bafc11a97e0ab34.jpg',
+      intro: '',
+      tag: 'Satisfied'
+    },
+    {
+      img: '',
+      intro: '',
+      tag: ''
+    },
+    {
+      img: '',
+      intro: '',
+      tag: ''
+    }
+  ]
+
   return (
     <div>
       <Section1 />
--- 07-UI-Project/src/components/Section1/RightCard.jsx
+++ 07-UI-Project/src/components/Section1/RightCard.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
+import RightCardContent from './RightCardContent'
 
 const RightCard = () => {
     return (
         <div className='h-full w-80 rounded-4xl overflow-hidden relative'>
             <img className='h-full w-full object-cover' src="https://i.pinimg.com/736x/55/82/e3/5582e315cd1adbee00a58665b198eba2.jpg" alt="" />
-            <div className='absolute top-0 left-0 p-8 h-full w-full flex flex-col justify-between'>
-                <h2 className='bg-white text-2xl font-semibold rounded-full h-14 w-14 flex justify-center items-center '>1</h2>
-                <div>
-                    <p className='text-xl leading-normal text-white mb-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit ea officia, voluptatibus mollitia!</p>
-                    <div className='flex justify-between'>
-                        <button className='bg-blue-600 text-white font-medium px-8 py-2 rounded-full'>Satisfied</button>
-                        <button className='bg-blue-600 text-white font-medium px-3 py-2 rounded-full'><i className="ri-arrow-right-line"></i></button>
-                    </div>
-                </div>
-            </div>
 
+            <RightCardContent />
         </div>
     )
 }
