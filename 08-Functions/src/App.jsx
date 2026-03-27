ba0b1f24a908339eefc9cb53062077f2a2bf88a3
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-27 10:10:11 +0530

    feat: implement button functionality to change header text and log mouse events

--- 08-Functions/src/App.jsx
+++ 08-Functions/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const App = () => {
+
+  // Event handler functions
+  function clickBtn() {
+    let h1 = document.querySelector('h1')
+    h1.textContent = 'Hello, Boss'
+  }
+
+  // Mouse enter event handler
+  function mouseEnter() {
+    console.log('Mouse entered');
+  }
+
   return (
     <div>
-      
+
+      {/* The value of h1 will be changed when the button is clicked */}
+      <h1>Hello, Lakshaya</h1>
+
+      {/* Button with mouse enter and click events */}
+      <button onMouseEnter={mouseEnter} onClick={clickBtn}>Change user</button>
     </div>
   )
 }
