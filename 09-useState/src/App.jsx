29769feb25a10d45f0961fa61077ac3c7b3ea1e4
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-30 12:59:52 +0530

    feat: refactor App component to use useState for state management and update value on button click

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
   // Can't change the value directly like this, it will not re-render the component because React doesn't know that the value has changed. It will only re-render when the state changes.
-  let a = 20;
+  // let a = 20;
+
+
+  
+  // useState: It is a hook that allows us to add state to functional components. It returns an array with two elements: the current state value and a function to update that value. When we call the update function, React will re-render the component with the new state value.
+  let [a, setA] = useState(20);
 
   function changeA() {
-    console.log(a);
-    a = 30;
-    console.log(a);
+    setA(a+1);
     
   }
 
