a1dca4de95070f312ef0e21f217fb2491dc503c9
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-03-30 13:09:36 +0530

    feat: refactor App component to implement increase and decrease functionality with buttons

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -6,19 +6,25 @@ const App = () => {
   // let a = 20;
 
 
-  
-  // useState: It is a hook that allows us to add state to functional components. It returns an array with two elements: the current state value and a function to update that value. When we call the update function, React will re-render the component with the new state value.
-  let [a, setA] = useState(20);
 
-  function changeA() {
+  // useState: It is a hook that allows us to add state to functional components. It returns an array with two elements: the current state value and a function to update that value. When we call the update function, React will re-render the component with the new state value.
+  let [a, setA] = useState(0);
+
+  function increaseA() {
     setA(a+1);
-    
+  }
+
+  function decreaseA() {
+    setA(a-1);
   }
 
   return (
     <div>
       <h1>The value of a is : {a}</h1>
-      <button onClick={changeA}>Change value</button>
+      <div className='btn'>
+        <button onClick={increaseA}>Increase value</button>
+      <button onClick={decreaseA}>Decrease value</button>
+      </div>
     </div>
   )
 }
