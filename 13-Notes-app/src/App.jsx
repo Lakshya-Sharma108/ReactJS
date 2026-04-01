36e0d59ae355ca2944286864aa45c27a325c7a6d
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-01 11:25:03 +0530

    feat: enhance form layout and styling for notes input

--- 13-Notes-app/src/App.jsx
+++ 13-Notes-app/src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <form className='flex justify-between'>
+    <div className='h-screen bg-black text-white'>
+      <form className='flex flex-col items-start gap-4 p-10'>
 
         <input
           type="text"
           placeholder='Enter Notes Heading'
+          className='px-5 py-2 border-2 rounded w-1/2'
         />
 
         <input
           type="text"
-          className=''
+          placeholder='Write Details'
+          className='px-5 py-2 h-20 border-2 rounded w-1/2'
         />
+
+        <button className='bg-white text-black px-5 py-2 rounded w-1/2'>Add Notes</button>
 
       </form>
     </div>
