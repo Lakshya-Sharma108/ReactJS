df65d273fae027dd656574eede6d5d37f659712f
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-01 09:38:41 +0530

    feat: implement form submission handling and add basic styling for input and button

--- 11-FormHandling/src/App.jsx
+++ 11-FormHandling/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const App = () => {
+
+  const handleSubmit = () => {
+    console.log('Form submitted!');
+    
+  }
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your name" />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
