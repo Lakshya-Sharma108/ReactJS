78e7a20906e2d7053af5b1602decf14d8316ebfe
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-01 10:17:46 +0530

    feat: initialize Notes app with React, Vite, and Tailwind CSS
    - Add main application structure in App.jsx
    - Create CSS styles for the app in App.css and index.css
    - Include SVG icons for various functionalities
    - Set up Vite configuration for React and Tailwind CSS
    - Add hero image and React logo assets
    - Implement responsive design for the layout

---
+++ 13-Notes-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form className='flex justify-between'>
+
+        <input
+          type="text"
+          placeholder='Enter Notes Heading'
+        />
+
+        <input
+          type="text"
+          className=''
+        />
+
+      </form>
+    </div>
+  )
+}
+
+export default App
