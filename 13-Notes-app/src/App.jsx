c4018ec1504c9d429c67f6521ec71b7ada5b95f8
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-01 17:27:38 +0530

    feat: restructure form layout for improved styling and responsiveness

--- 13-Notes-app/src/App.jsx
+++ 13-Notes-app/src/App.jsx
@@ -5,30 +5,38 @@ const App = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     console.log('form submitted');
-    
+
   }
 
   return (
-    <div className='h-screen bg-black text-white'>
-      <form onSubmit={submitHandler} className='flex items-start justify-between p-10'>
+    <div className='h-screen lg:flex bg-black text-white'>
+      <form onSubmit={submitHandler} className='flex gap-4 lg:w-1/2 p-10 items-start flex-col'>
 
-        <div className='w-1/2 flex items-start flex-col gap-4'>
-          <input
-            type="text"
-            placeholder='Enter Notes Heading'
-            className='px-5 py-2 border-2 rounded font-medium outline-none w-full'
-          />
+        <input
+          type="text"
+          placeholder='Enter Notes Heading'
+          className='px-5 py-2 border-2 rounded font-medium outline-none w-full'
+        />
 
-          <textarea
-            type="text"
-            placeholder='Write Details'
-            className='px-5 py-2 h-32 border-2 rounded font-medium outline-none w-full'
-          />
+        <textarea
+          type="text"
+          placeholder='Write Details'
+          className='px-5 py-2 h-32 border-2 rounded font-medium outline-none w-full'
+        />
 
-          <button className='bg-white text-black px-5 py-2 rounded font-medium outline-none w-full'>Add Notes</button>
-        </div>
+        <button className='bg-white text-black px-5 py-2 rounded font-medium outline-none w-full'>Add Notes</button>
 
       </form>
+
+      <div className=' lg:w-1/2 border-l-2 p-10'>
+        <h1 className='text-3xl font-bold'>Your Notes</h1>
+        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        </div>
+      </div>
     </div>
   )
 }
