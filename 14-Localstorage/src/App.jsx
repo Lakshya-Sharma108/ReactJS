dda8fba50cf621534a78adea43ad7d799b81466e
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-05 12:22:37 +0530

    feat: comment out local storage examples for clarity and maintainability

--- 14-Localstorage/src/App.jsx
+++ 14-Localstorage/src/App.jsx
@@ -8,24 +8,42 @@ const App = () => {
 
   // How to use local storage using methods?
   // 1. setItem(key, value)
-  localStorage.setItem('name', 'Lakshaya')
-  localStorage.setItem('age', '21')
-  localStorage.setItem('city', 'Delhi')
+  // localStorage.setItem('name', 'Lakshaya')
+  // localStorage.setItem('age', '21')
+  // localStorage.setItem('city', 'Delhi')
 
 
   // 2. getItem(key)
-  const name = localStorage.getItem('name')
-  const age = localStorage.getItem('age')
-  const city = localStorage.getItem('city')
-  console.log(name, age, city)  // check on console
+  // const name = localStorage.getItem('name')
+  // const age = localStorage.getItem('age')
+  // const city = localStorage.getItem('city')
+  // console.log(name, age, city)  // check on console
 
   
   // 3. removeItem(key)
-  localStorage.removeItem('city')
-
+  // localStorage.removeItem('city')
 
   // 4. clear() - This method is used to clear all the data stored in local storage.
-  localStorage.clear()
+  // localStorage.clear()
+
+
+
+
+  const user = {
+    name: 'Lakshaya',
+    age: 21,
+    city: 'Delhi'
+  }
+
+  // We cannot store objects in local storage directly, we need to convert them to strings using JSON.stringify() method.
+  // localStorage.setItem('user', JSON.stringify(user))
+
+  const userData = localStorage.getItem('user')
+  console.log(userData)  // Now it will give me in string format, but want it in object format, so we need to parse it using JSON.parse() method.
+
+  console.log(JSON.parse(userData));
+  
+
 
 
 
