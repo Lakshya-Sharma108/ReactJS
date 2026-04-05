e0b9314b3b9999d676250591314c8f78e49941b5
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-05 13:08:14 +0530

    feat: add axios dependency and update App component to demonstrate API fetching with axios

--- 15-API-Calling/src/App.jsx
+++ 15-API-Calling/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
 
@@ -12,24 +13,43 @@ const App = () => {
   // We have two ways to fetch data from an API in React:  1. fetch() method  2. axios library
 
 
+  // Difference between fetch() method and axios library:
+  // 1. fetch() is a built-in JavaScript method, while axios is a third-party library.
+  // 2. fetch() returns a promise that resolves to the Response object, while axios returns a promise that resolves to the response data.
+  // 3. fetch() does not automatically transform the response data, while axios automatically transforms the response data to JSON.
+  // 4. fetch() does not support request cancellation, while axios supports request cancellation using CancelToken.
+  // 5. fetch() does not support interceptors, while axios supports interceptors for request and response.
+  // 6. fetch() does not support older browsers, while axios supports older browsers.
+  // 7. fetch() does not have built-in support for handling timeouts, while axios has built-in support for handling timeouts.
+  // 8. fetch() does not have built-in support for handling errors, while axios has built-in support for handling errors.
 
+
+  
 
   // Using fetch method:
   async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/5')
     const data = await response.json()
 
     console.log(data)
   }
 
 
-
+  // Using axios library:
+  const getDataa = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/10')
+    console.log(response.data);
+    
+    
+  }
 
 
 
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
+      <button onClick={getData}>Get Data  --fetch method</button>
+
+      <button onClick={getDataa}>Get Data  --axios library</button>
     </div>
   )
 }
