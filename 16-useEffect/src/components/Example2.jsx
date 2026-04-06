e23323edbede9f2c3968329243ae54268c324ff5
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-06 11:26:01 +0530

    feat: add state management and useEffect hook to Example2 component

--- 16-useEffect/src/components/Example2.jsx
+++ 16-useEffect/src/components/Example2.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Example2 = () => {
+
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
   function aChange() {
     console.log('A ki value change ho gayi');
@@ -10,9 +13,31 @@ const Example2 = () => {
     console.log('B ki value change ho gayi');
   }
 
+  useEffect(() => {
+    console.log('use effect is running...')
+  }, [a]);
+
   return (
-    <div className='flex flex-col justify-center items-center gap-5 h-screen w-screen bg-sky-950 text-white'>
-      
+    <div className='flex flex-col justify-center items-center gap-5 h-screen w-screen bg-sky-950 text-white text-2xl font-bold'>
+
+      <h1>A is -- {a}</h1>
+      <h1>B is -- {b}</h1>
+
+      <div className='flex gap-5 text-lg font-normal'>
+        <button
+          className='py-3 px-6 rounded-3xl bg-gray-500'
+          onClick={() => {
+            setA(a + 1);
+          }}
+
+        >Change A</button>
+        <button
+          className='py-3 px-6 rounded-3xl bg-gray-500'
+          onClick={() => {
+            setB(b - 1);
+          }}
+        >Change B</button>
+      </div>
     </div>
   )
 }
