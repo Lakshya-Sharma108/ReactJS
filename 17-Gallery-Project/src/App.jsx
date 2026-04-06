b08a5e998e7e177117b6cd16358fd7c8ad5251ef
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-06 12:48:36 +0530

    feat: update data fetching limit and enhance user data display in App component

--- 17-Gallery-Project/src/App.jsx
+++ 17-Gallery-Project/src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   const [userData, setUserData] = useState([]);
 
   const getData = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
-    
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=50')
+
     setUserData(response.data)
 
     console.log(userData);
@@ -17,19 +17,27 @@ const App = () => {
 
   if (userData.length > 0) {
     printUserData = userData.map((elem, idx) => {
-      return idx
+
+      return <div key={idx}>
+        <a href={elem.url}>
+          <div className='h-40 w-44 overflow-hidden rounded'>
+            <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
+          </div>
+          <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
+      </div>
     })
   }
 
   return (
-    <div className='bg-black h-screen p-4 text-white'>
+    <div className='bg-black h-screen overflow-auto p-4 text-white'>
       <button
         onClick={getData}
         className='bg-green-600 active:scale-95 px-5 py-2 rounded '
       >
-      Get Data</button>
+        Get Data</button>
 
-      <div>
+      <div className='flex flex-wrap gap-4'>
         {printUserData}
       </div>
     </div>
