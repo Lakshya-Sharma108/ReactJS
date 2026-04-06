983ef49fe90a59405d1a4d7a8157f0d9aec8b3f2
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-06 13:19:32 +0530

    feat: implement pagination and improve data fetching in App component

--- 17-Gallery-Project/src/App.jsx
+++ 17-Gallery-Project/src/App.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
   const [userData, setUserData] = useState([]);
 
+  const [index, setIndex] = useState(1);
+
   const getData = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=50')
-
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=12`)
     setUserData(response.data)
-
-    console.log(userData);
   }
 
-  let printUserData = 'No User Available'
+  useEffect(() => {
+    getData()
+  }, [index]);
+
+
+
+  let printUserData = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>loading...</h3>
 
   if (userData.length > 0) {
     printUserData = userData.map((elem, idx) => {
 
       return <div key={idx}>
-        <a href={elem.url}>
-          <div className='h-40 w-44 overflow-hidden rounded'>
+        <a href={elem.url} target='_blank'>
+          <div className='h-48 w-47 overflow-hidden rounded'>
             <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
           </div>
           <h2 className='font-bold text-lg'>{elem.author}</h2>
@@ -29,16 +34,32 @@ const App = () => {
     })
   }
 
+
   return (
     <div className='bg-black h-screen overflow-auto p-4 text-white'>
-      <button
-        onClick={getData}
-        className='bg-green-600 active:scale-95 px-5 py-2 rounded '
-      >
-        Get Data</button>
-
-      <div className='flex flex-wrap gap-4'>
+      <h1>{index}</h1>
+      <div className='flex flex-wrap gap-4 p-2'>
         {printUserData}
+      </div>
+
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
+          onClick={()=>{
+            if(index > 1){
+              setIndex(index-1)
+              setUserData([])
+            }
+          }}
+        >Prev</button>
+
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
+          onClick={()=>{
+            setUserData([])
+            setIndex(index+1)
+          }}
+        >Next</button>
       </div>
     </div>
   )
