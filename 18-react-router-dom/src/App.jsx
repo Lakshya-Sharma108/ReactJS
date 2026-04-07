dca6d77f3f0a0a5b04cd50b739cf3f88a5b744a7
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-07 08:55:02 +0530

    feat: add Navbar component and integrate it into App component

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -3,10 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
