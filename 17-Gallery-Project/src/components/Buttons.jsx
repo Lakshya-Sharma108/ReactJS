41cb678c3181d5e85d97872547cfeef742b524d8
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-07 08:16:22 +0530

    fix: adjust opacity style for previous button in Buttons component

--- 17-Gallery-Project/src/components/Buttons.jsx
+++ 17-Gallery-Project/src/components/Buttons.jsx
@@ -5,6 +5,7 @@ const Buttons = (props) => {
         <div>
             <div className='flex justify-center gap-6 items-center mt-7 p-4'>
                 <button
+                    style={{opacity: props.index == 1 ? 0.5 : 1}}
                     className='bg-amber-400 text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
                     onClick={() => {
                         if (props.index > 1) {
