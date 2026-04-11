c513b87ddde61634775f7cdb646bbbc8fa8e9380
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-11 13:13:23 +0530

    style: add comments explaining the use of Link component in Navbar

--- 19-routing-advance/src/components/Navbar.jsx
+++ 19-routing-advance/src/components/Navbar.jsx
@@ -6,6 +6,11 @@ const Navbar = () => {
     <div className='flex justify-between items-center py-4 px-8 bg-cyan-900'>
       <h2 className='text-xl font-bold'>Lakshaya</h2>
       <div className='flex gap-8'>
+
+        {/* 
+            Why to use Link instead of anchor tag <a> in react router dom?
+            Ans: Because when we use anchor tag <a> it will reload the whole page and we will lose the state of our application but when we use Link it will not reload the page and it will only change the url and render the component which is associated with that url.
+        */}
         <Link className='text-lg font-medium' to="/">Home</Link>
         <Link className='text-lg font-medium' to="/about">About</Link>
         <Link className='text-lg font-medium' to="/product">Product</Link>
