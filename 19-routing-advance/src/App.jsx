0f21a4aabe636b21946bc17722c7f94ea1f8f62e
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-11 13:11:52 +0530

    feat: add Men and Women pages with routing and update Navbar links to use Link component

--- 19-routing-advance/src/App.jsx
+++ 19-routing-advance/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import Product from './pages/Product'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Men from './pages/Men'
+import Women from './pages/Women'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -16,6 +18,10 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/product' element={<Product />} /> 
+
+        {/* Nestead Routes */}
+        <Route path='/product/men' element={<Men />} />
+        <Route path='/product/women' element={<Women />} />
 
         {/* This is for 404 pages which are not exists in our application */}
         <Route path='*' element={<NotFound />} />
--- 19-routing-advance/src/components/Navbar.jsx
+++ 19-routing-advance/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-4 px-8 bg-cyan-900'>
       <h2 className='text-xl font-bold'>Lakshaya</h2>
       <div className='flex gap-8'>
-        <a className='text-lg font-bold' href="/">Home</a>
-        <a className='text-lg font-bold' href="/about">About</a>
-        <a className='text-lg font-bold' href="/product">Product</a>
+        <Link className='text-lg font-medium' to="/">Home</Link>
+        <Link className='text-lg font-medium' to="/about">About</Link>
+        <Link className='text-lg font-medium' to="/product">Product</Link>
       </div>
     </div>
   )
